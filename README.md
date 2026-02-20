190de8d3dbbb8b33723e7af6d3d230712fb6b7b6
Walter Alexander Gonzabay Rojas <wagonzab@espol.edu.ec>
2026-02-20 10:16:23 -0500

    add function to take pics

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 here talk about the principal documentation and add reference for the other docs
 
 ### instalation
-"" 
+"""
     pip install -r requirements.txt
-""
+
+"""
--- src/calibration/calibration.py
+++ src/calibration/calibration.py
@@ -1,6 +1,29 @@
-import cv2
+import cv2 as cv
 import numpy as np
 
 
 
+cap = cv.VideoCapture(0) #change if it is neccesary 
+
+def take_pics():
+    ret, frame = cap.read()
+    return frame if ret else -1
+
+
+
+def main():
+    result = take_pics()
+
+    while True:
+        if result != -1:
+            cv.imshow("camera", result)
+
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+        
+
+if __name__ == "__main__":
+    main()
+    cap.release()
+    cv.destroyAllWindows()
 
