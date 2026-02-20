bc54db4d116990a884048a58adcc155e1e7b0f13
Walter Alexander Gonzabay Rojas <wagonzab@espol.edu.ec>
2026-02-20 10:44:31 -0500

    generating differents figures

--- src/calibration/calibration.py
+++ src/calibration/calibration.py
@@ -1,26 +1,36 @@
 import cv2 as cv
 import numpy as np
+from time import sleep
+import file
 
-
+destination = "./../../tmp/figs/calibration/"
 
 cap = cv.VideoCapture(0) #change if it is neccesary 
 
-def take_pics():
-    ret, frame = cap.read()
-    return frame 
+def take_pics(num_pics = 30):
+
+    cont_pic = 0
+
+    print("##### Tomando fotos para la calibracion")
+    pics = []
+    while cont_pic < num_pics:
+        ret, frame = cap.read()  
+        if ret: pics.append(frame) else break
+
+        cont_pic += 1
+
+        cv.imwrite(destination + "/figure" + str(cont_pic) + ".jpg", frame)
+        print(f"# foto numero {cont_pic} tomada")
+
+        sleep(1)
+        if cont_pic == 30: break
+    
+    return pics 
 
 
 
 def main():
     result = take_pics()
-
-    while True:
-        
-        cv.imshow("camera", result)
-
-        if cv.waitKey(1) & 0xFF == ord('q'):
-            break
-        
 
 if __name__ == "__main__":
     main()
